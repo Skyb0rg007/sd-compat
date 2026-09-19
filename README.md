9400825920466fde3118afd68555da3b2993ed15
Skye Soss <skye@soss.website>
2026-09-19 18:43:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2026 Skye Soss <skye@soss.website>
+
+SPDX-License-Identifier: LGPL-2.1-or-later
+-->
+
+# sd-compat
+
+A libsystemd compatibility library.
---
+++ shell.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2026 Skye Soss <skye@soss.website>
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  packages = [
+    pkgs.reuse
+  ];
+}
